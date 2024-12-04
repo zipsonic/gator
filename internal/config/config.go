7881a9006d40981b5bcaa03e31d6b3a08565832e
zipsonic <rick@hail3.com>
2024-12-04 07:29:05 -0800

    updates

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,12 @@
 package config
 
+const CONFIGFILE string = ".gatorconfig.json"
+
 type Config struct {
-	DBUrl           string
-	CurrentUserName string
+	DBUrl           string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
+}
+
+func Read() (Config, error) {
+
 }
