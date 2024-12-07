79b94f0a04ebbbb8b2b11b9c8f07c8fc3979b15c
zipsonic <rick@hail3.com>
2024-12-06 19:25:56 -0800

    updated

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,14 +1,43 @@
 package config
 
+import (
+	"encoding/json"
+	"os"
+)
+
 const CONFIGFILE string = ".gatorconfig.json"
 
 type Config struct {
-	DBUrl           string `json:"db_url"`
-	CurrentUserName string `json:"current_user_name"`
+	DBUrl           *string `json:"db_url"`
+	CurrentUserName *string `json:"current_user_name"`
 }
 
 func Read() (Config, error) {
+
 	var config Config
+	configpath, err := os.UserHomeDir()
+	if err != nil {
+		return config, err
+	}
+	configfile := configpath + "/" + CONFIGFILE
+
+	file, err := os.Open(configfile)
+	if err != nil {
+		return config, err
+	}
+
+	defer file.Close()
+
+	body := make([]byte, 0)
+
+	_, err = file.Read(body)
+	if err != nil {
+		return config, err
+	}
+
+	if err = json.Unmarshal(body, &config); err != nil {
+		return config, err
+	}
 
 	return config, nil
 }
